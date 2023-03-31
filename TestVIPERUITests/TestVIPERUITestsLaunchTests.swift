1e8949ed2b0b939f269f1c79d64e8acd86340cd0
Vladimir Kravets <natccorp@gmail.com>
2023-03-31 11:39:18 +0300

    Initial Commit

---
+++ TestVIPERUITests/TestVIPERUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TestVIPERUITestsLaunchTests.swift
+//  TestVIPERUITests
+//
+//  Created by Vladimir Kravets on 31.03.2023.
+//
+
+import XCTest
+
+final class TestVIPERUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
